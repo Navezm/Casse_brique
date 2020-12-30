69b1e39d27a4bc163a74c8ad0592b558e91295de
Navezm <navez.martin@gmail.com>
2020-12-30 16:36:25 +0100

    Bricks en cours

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,60 @@
+// Déclaration des éléments
 let canvas = document.getElementById("myCanva");
 let ctx = canvas.getContext("2d");
 let x = canvas.width/2;
 let y = canvas.height-15;
 let dx = 2;
 let dy = -2;
-let ballRadius = 5
-let paddleHeight = 10;
-let paddleWidth = 75;
+let ballRadius = 5;
+let paddleHeight = 5;
+let paddleWidth = 50;
 let paddleX = (canvas.width-paddleWidth)/2;
 let rightPress = false;
 let leftPress = false;
+
+// Déclaration des briques
+let brickRowCount = 5;
+let brickColumnCount = 9;
+let brickWidth = 25;
+let brickHeight = 5;
+let brickPadding = 7;
+let brickOffsetTop = 10;
+let brickOffsetLeft = 10;
+
+let bricks = [];
+for (let i = 0; i < brickColumnCount; i++){
+    bricks[i] = [];
+    for (let e = 0; e < brickColumnCount; e++){
+        bricks[i][e] = {x: 0, y: 0};
+    };
+};
+
+let drawBrick = () => {
+    for (let i = 0; i < brickColumnCount; i++){
+        for (let e = 0; e < brickRowCount; e++){
+            // if(i == 0){
+            //     for (let a = 0; a < 2; a++) {
+            //         let brickX = (i*(brickWidth+brickPadding) + brickOffsetLeft);
+            //         let brickY = (e*(brickHeight+brickPadding) + brickOffsetTop);
+            //         bricks[i][e].x = brickX;
+            //         bricks[i][e].y = brickY;
+            //         ctx.beginPath();
+            //         ctx.fillRect(brickX, brickY, brickWidth, brickHeight);
+            //         ctx.fillStyle = "white";
+            //         ctx.closePath();
+            //     }
+            // }
+            let brickX = (i*(brickWidth+brickPadding) + brickOffsetLeft);
+            let brickY = (e*(brickHeight+brickPadding) + brickOffsetTop);
+            bricks[i][e].x = brickX;
+            bricks[i][e].y = brickY;
+            ctx.beginPath();
+            ctx.fillRect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "white";
+            ctx.closePath();
+        };
+    };
+};
 
 // Bouger le paddle
 document.addEventListener("keydown", function(e){
@@ -32,7 +77,9 @@ document.addEventListener("keyup", function(e){
 let drawBall = () => {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.strokeStyle = "green";
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.closePath();
 };
@@ -41,8 +88,10 @@ let drawBall = () => {
 let drawPaddle = () => {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "white";
     ctx.fill();
+    ctx.strokeStyle = "black";
+    ctx.stroke();
     ctx.closePath();
 };
 
@@ -52,11 +101,23 @@ let draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBrick();
 
     // Rebond balle
-    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+    // Axe Y
+    if (y + dy < ballRadius){
         dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius ){
+        dy = -dy;
+        // if(x > paddleX && x < paddleX + paddleWidth){
+        //     dy = (-dy - 0.2);
+        // } else {
+        //     alert("GAME OVER");
+        //     document.location.reload();
+        //     clearInterval(interval);
+        // };
     };
+    // Axe X
     if (x + dx > canvas.width-ballRadius || x + dx < ballRadius){
         dx = -dx;
     };
@@ -74,8 +135,9 @@ let draw = () => {
         };
     };
 
+    // Mouvement de la balle
     x += dx;
     y += dy;
 };
 
-setInterval(draw, 15);
+let interval = setInterval(draw, 15);
